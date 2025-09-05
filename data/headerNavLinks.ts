7737edf53f948d29fcc81ba0dc21b90397e8a034
hcp825 <hechaoping@gmail.com>
2025-09-05 11:22:54 +0800

    更新 headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'News' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/projects', title: 'Product' },
   { href: '/about', title: 'About' },
 ]
 
